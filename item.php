8b2fedb8c9c1c6ff0c976ca5687c39378dee60c2
MonelThibault <thibault.monel@le-campus-numerique.fr>
2022-05-02 11:50:55 +0200

    item finished

---
+++ item.php
@@ -0,0 +1,8 @@
+<?php
+$product_name = "Jester Hat";
+$price = "66$";
+$product_pic ="https://emojis.slackmojis.com/emojis/images/1643516618/26601/jester-hat.png?1643516618";
+
+echo $product_name;
+echo "<br><img src=$product_pic alt='hat pic'>";
+echo "<br>". $price;
