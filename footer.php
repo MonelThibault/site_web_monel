b4e19c200a4074fd74f13d18fe7edd5883e54919
MonelThibault <thibault.monel@le-campus-numerique.fr>
2022-05-02 13:23:45 +0200

    update with website

---
+++ footer.php
@@ -0,0 +1,24 @@
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Referencement -->
+    <meta name="description" content="Chercher les meilleures prisons et les matons les plus hot de votre région">
+    <meta name="keywords" content="Accueil, Prison, arnaque, prison, adopt1maton">
+    <title>Prison Advisor : recherche de prison et de maton</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/styles_header.css">
+    <link rel="shortcut icon" type="image/x-icon" href="./assets/favicons/favicon.ico" />
+</head>
+
+<!-- FOOTER -->
+<footer class="container-fluid">
+    <p class="fs-4 mb-0">Tous droits réservés - Prison Advisor - Mentions légales 2022</p>
+</footer>
