f2710520a76122a8fce2003063147f750f69e1f1
MonelThibault <thibault.monel@le-campus-numerique.fr>
2022-05-02 13:32:56 +0200

    Update renaming files

--- item.php
+++ item.php
@@ -1,8 +1,10 @@
 <?php
 $product_name = "Jester Hat";
 $price = "66$";
-$product_pic ="https://emojis.slackmojis.com/emojis/images/1643516618/26601/jester-hat.png?1643516618";
-
+$product_pic = "https://emojis.slackmojis.com/emojis/images/1643516618/26601/jester-hat.png?1643516618";
+include 'header.php';
 echo $product_name;
 echo "<br><img src=$product_pic alt='hat pic'>";
-echo "<br>". $price;
+echo "<br>" . $price;
+include 'main.php';
+include 'footer.php';
