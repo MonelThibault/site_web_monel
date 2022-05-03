550f08eb8672694df485efd8c5088e59192a9e9f
MonelThibault <thibault.monel@le-campus-numerique.fr>
2022-05-03 09:57:40 +0200

    Update catalog and item 3.1

--- item.php
+++ item.php
@@ -2,6 +2,7 @@
 $product_name = "Jester Hat";
 $price = "66$";
 $product_pic = "https://emojis.slackmojis.com/emojis/images/1643516618/26601/jester-hat.png?1643516618";
+
 include 'header.php';
 echo $product_name;
 echo "<br><img src=$product_pic alt='hat pic'>";
---
+++ simple-catalog.php
@@ -0,0 +1,5 @@
+<?php
+
+$products = ["Menottes","Matraque","Surin"];
+sort ($products);
+print_r($products);
