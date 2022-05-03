5d45b53f3af6c4d52d99e03bfad5ca659b81670f
MonelThibault <thibault.monel@le-campus-numerique.fr>
2022-05-03 16:54:31 +0200

    Errata: simple-catalog done.

--- simple-catalog.php
+++ simple-catalog.php
@@ -3,3 +3,27 @@
 $products = ["Menottes","Matraque","Surin"];
 sort ($products);
 print_r($products);
+
+$tab_count = count($products);
+
+$i=0;
+while ($i < $tab_count){
+    echo $products[$i] . "\n" ;
+    $i++;
+}
+
+$i2=0;
+do{
+    echo $products[$i2] . "\n" ;
+    $i2++;
+}
+while($i2 < $tab_count);
+
+
+for($i3=0; $i3 < $tab_count; $i3++){
+    echo $products[$i3] . "\n" ;
+}
+
+foreach($products as $value){
+    echo $value . "\n" ;
+}
