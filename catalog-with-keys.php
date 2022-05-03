8a8f89d69c630bca44dc25fb958f2e1ccd5ada7a
MonelThibault <thibault.monel@le-campus-numerique.fr>
2022-05-03 12:21:19 +0200

    update 3.2 catalog-with-keys.php

---
+++ catalog-with-keys.php
@@ -0,0 +1,50 @@
+<html>
+<title> Vente de truc et bidules</title>
+<body>
+<div>
+    <h3> Menottes </h3>
+    <p> Prix : 45€ </p>
+    <img
+        src="https://static.wikia.nocookie.net/fortnite/images/9/9a/Menottes.png/revision/latest?cb=20200221003955&path-prefix=fr">
+</div>
+
+<div>
+    <h3> Matraque </h3>
+    <p> Prix : 60€ </p>
+    <img
+        src="https://www.fightpremium.com/media/catalog/product/cache/1/small_image/256x256/9df78eab33525d08d6e5fb8d27136e95/m/a/matraque/ESP-Baton-Matraque-telescopique-noire-54-cm-(21-pouces)-Poignee-ergonomique-22.jpg">
+</div>
+
+<div>
+    <h3> Surin </h3>
+    <p> Prix : 90€ </p>
+    <img src="https://art.hearthstonejson.com/v1/256x/BAR_322.jpg">
+</div>
+</body>
+
+</html>
+
+<?php
+$menottes = [
+    "name" => "Menottes",
+    "price" => 45 . "euros",
+    "weight" => 600 . "g",
+    "discount" => "10%",
+    "picture_url" => "https://static.wikia.nocookie.net/fortnite/images/9/9a/Menottes.png/revision/latest?cb=20200221003955&path-prefix=fr",
+];
+
+$matraque = [
+    "name" => "Matraque",
+    "price" => 60 . "euros",
+    "weight" => 1.5 . "kg",
+    "discount" => "10%",
+    "picture_url" => "https://www.fightpremium.com/media/catalog/product/cache/1/small_image/256x256/9df78eab33525d08d6e5fb8d27136e95/m/a/matraque/ESP-Baton-Matraque-telescopique-noire-54-cm-(21-pouces)-Poignee-ergonomique-22.jpg",
+];
+
+$surin = [
+    "name" => "Surin",
+    "price" => 90 . "euros",
+    "weight" => 6 . "g",
+    "discount" => null,
+    "picture_url" => "https://art.hearthstonejson.com/v1/256x/BAR_322.jpg",
+];
