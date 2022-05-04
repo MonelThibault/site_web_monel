10ef570d244f719a45f2581a3b7d2119b0336667
MonelThibault <thibault.monel@le-campus-numerique.fr>
2022-05-04 16:27:53 +0200

    5.1 initial update.

--- item.php
+++ item.php
@@ -1,11 +1,30 @@
-<?php
-$product_name = "Jester Hat";
-$price = "66$";
-$product_pic = "https://emojis.slackmojis.com/emojis/images/1643516618/26601/jester-hat.png?1643516618";
 
-include 'header.php';
-echo $product_name;
-echo "<br><img src=$product_pic alt='hat pic'>";
-echo "<br>" . $price;
-include 'main.php';
-include 'footer.php';
+//$product_name = "Jester Hat";
+//$price = "66$";
+//$product_pic = "https://emojis.slackmojis.com/emojis/images/1643516618/26601/jester-hat.png?1643516618";
+<html>
+<title> Vente de truc et bidules</title>
+<body>
+
+<?php
+
+include 'multidimensional-catalog.php';
+include 'my-functions.php';
+
+
+
+
+
+
+
+
+//include 'header.php';
+//echo $product_name;
+//echo "<br><img src=$product_pic alt='hat pic'>";
+//echo "<br>" . $price;
+//include 'main.php';
+//include 'footer.php';
+?>
+
+</body>
+</html>
---
+++ my-functions.php
@@ -0,0 +1,6 @@
+<?php
+
+
+function formatPrice($price){
+
+}
