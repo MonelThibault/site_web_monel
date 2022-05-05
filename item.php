dc7cb5ba359f29bebc4f812ed60e583c1419535b
MonelThibault <thibault.monel@le-campus-numerique.fr>
2022-05-05 11:46:12 +0200

    5.3: discountedPrice done + re-arranging code on item and multidimensional-catalog.

--- item.php
+++ item.php
@@ -7,13 +7,7 @@ include 'my-functions.php';
 include 'multidimensional-catalog.php';
 
 
-
-
-
 //include 'header.php';
-//echo $product_name;
-//echo "<br><img src=$product_pic alt='hat pic'>";
-//echo "<br>" . $price;
 //include 'main.php';
 //include 'footer.php';
 ?>
--- my-functions.php
+++ my-functions.php
@@ -1,5 +1,11 @@
 <?php
 
+function discountedPrice($price_to_discount, $discount){
+    $discounted_price = $discount * $price_to_discount/100;
+    $discounted_price = formatPrice($discounted_price);
+    return $discounted_price;
+}
+
 function priceExcludingVAT($price){
     $price_without_taxes = (100 * $price)/(100 + 20);
     $price_without_taxes = formatPrice($price_without_taxes);
@@ -9,5 +15,5 @@ function priceExcludingVAT($price){
 function formatPrice($price_to_format){
     $price_to_format = $price_to_format/100;
     $formatted_price = number_format($price_to_format,2, ',', ' ');
-return $formatted_price . "€";
+    return $formatted_price . "€";
 }
